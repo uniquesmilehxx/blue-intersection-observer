87a0d0b7883e34929e389cd802667b88d3ddfd58
haoxiaoxiao <haoxiaoxiao@netjoy.com>
2020-12-01 14:42:30 +0800

    intoViewActivate-version-1.0.0

---
+++ page/observer.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @param {eleClassName} String: initial className of container
+ * @param {activeClassName} String: the className you need to add if the container come into view 
+ * @param {isOnceTime: true} Boolean: is once-time or not(every-time)
+ * 
+ */
+function intoViewActivate ({eleClassName, activeClassName, isOnceTime = true}) {
+    
+    if (!eleClassName || !activeClassName) return
+
+    let io = new IntersectionObserver( function (entris) {
+        entris.forEach( entry => {
+            // 若进入视野（intersectionRatio：目标元素的可见比例）
+            if (entry.intersectionRatio > 0) {  
+                let classString = entry.target.getAttribute('class')  
+                if (classString.indexOf(activeClassName) < 0) {   
+                    entry.target.classList.add(activeClassName)
+                } 
+            } else if (!isOnceTime){
+                entry.target.classList.remove(activeClassName)
+            }
+        })
+    })
+
+    let eles = document.querySelectorAll(`.${eleClassName}`)
+    eles.forEach( item => {
+        io.observe(item)
+    })
+}
